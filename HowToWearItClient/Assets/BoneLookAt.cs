10340e693bbe36ed599c5c74dd040bd985f84f42
James Coote <coote.james@gmail.com>
2019-06-11 21:48:37 +0200

    Added client and server unity projects.

---
+++ HowToWearItClient/Assets/BoneLookAt.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoneLookAt : MonoBehaviour
+{
+    public GameObject startPoint;
+    public GameObject endPoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookAt = endPoint.transform.position - startPoint.transform.position;
+        transform.forward = lookAt.normalized;
+        transform.position = startPoint.transform.position + ((endPoint.transform.position - startPoint.transform.position) / 2);
+        transform.localScale = new Vector3(1, 1, (Vector3.Distance(startPoint.transform.position, endPoint.transform.position) / 2));
+    }
+}
---
+++ HowToWearItKinect/Assets/BodyToPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Windows.Kinect;
+
+public class BodyToPlayer : MonoBehaviour
+{
+    public static Body body;
+    public JointType jointType;
+    private const float scaleFactor = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (body != null && body.Joints.ContainsKey(jointType))
+        {
+            Windows.Kinect.Joint joint = body.Joints[jointType];
+            transform.position = new Vector3(joint.Position.X * scaleFactor, joint.Position.Y * scaleFactor, joint.Position.Z * scaleFactor);
+        }
+    }
+}
